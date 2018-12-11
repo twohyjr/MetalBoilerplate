1d3f310bd70df832c2c3041acba083984a69e46c
twohyjr <twohyjr@gmail.com>
2018-12-11 11:54:35 -0500

    Updated Math to be plain ol math

--- MetalBoilerplate/Math.swift
+++ MetalBoilerplate/Math.swift
@@ -190,12 +190,3 @@ extension matrix_float4x4 {
     }
     
 }
-
-//let unitsWide: Float = 2
-//let unitsTall = GameView.Height / (GameView.Width / unitsWide)
-//return matrix_float4x4.orthographic(right: unitsWide / 2.0,
-//                                    left: -unitsWide / 2.0,
-//                                    top: unitsTall / 2.0,
-//                                    bottom: -unitsTall / 2.0,
-//                                    near: -10.0,
-//                                    far: 10.0)
